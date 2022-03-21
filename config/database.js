23d434ba8125ba76ecc48d7a045ee1cb8ef18be0
emiliasosa <emi.sosato@gmail.com>
2022-03-21 01:06:51 -0300

    inicio sesion, registro, crear libro

--- config/database.js
+++ config/database.js
@@ -13,6 +13,18 @@ function query(sql, data){
     return new Promise((resolve, reject) =>{
         connection.query(sql, data, (error, result)=>{
             if(error){
+                console.log(error)
+                if(error.errno === 1062){
+                    //split permite dividir los caracteres, divido el string, divido con comillas
+                    const errorData = error.sqlMessage.split("'")
+                    const value = errorData[1]
+                    //se divide lo de la posicion 3 por punto, de la posicion 1 de esa separacion de ., 
+                    //se divide en _ y se toma lo que esta en la posicion 0
+                    const field = errorData[3].split(".")[1].split("_")[0]
+
+                    const message = `The ${field} "${value}" is already in use.`
+                    reject(message)
+                }
                 reject(error.sqlMessage)
             }else{
                 resolve(result)
@@ -32,8 +44,9 @@ async function edit(user, id){
 
 async function insert(tableName, data){
     try{
-        await query (`INSERT INTO ${tableName}(??) VALUES(?)`, [Object.keys(data), Object.values(data)])
-        return {data, success: true}
+        const result = await query (`INSERT INTO ${tableName}(??) VALUES(?)`, [Object.keys(data), Object.values(data)])
+        //devuelve el id
+        return {success:true, id: result.insertId}
     }catch(error){
         return {error, success: false}
     }
