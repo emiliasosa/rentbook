34c4d3c588188d50a7e43595d3b31162b79a11ce
emiliasosa <emi.sosato@gmail.com>
2022-03-22 00:00:33 -0300

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -10,6 +10,10 @@ router.get("/books", bookController.getBookView)
 router.post("/books", bookController.bookUp)
 
 router.get("/rentbook", bookController.getBookView)
-router.post("/rentbook", bookController.rentBook)
+router.get("/rentbook/:idBook", bookController.rentBook)
+
+router.get("/mybooks", bookController.getMyBookView)
+router.post("/mybooks", bookController.addMyBook)
+router.post("/mybooks", bookController.deleteMyBook)
 
 module.exports = router
